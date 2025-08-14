c436fe2a5008279d3488c9746d36e3fad142253e
harishkumar <hk0790533@gmail.com>
2025-08-14 14:43:13 +0530

    day9 commit

---
+++ project 1/table.js
@@ -0,0 +1,35 @@
+// Get user input
+ 
+    
+   
+const name = prompt("Enter your name:");
+    const email = prompt("Enter your email:");
+    const number = prompt("Enter your phone number:");
+    const city = prompt("Enter your city:");
+    const  blood=prompt("enter your blood group") ;
+    const  native=prompt("enter your native place") ;
+     const dob =prompt("enter your date of birth") ;
+     const  pob=prompt("enter your place of birth") ;
+    const sslcmark =prompt("enter your sslc mark") ;
+    
+    const  mark=prompt("enter your 12th mark") ; 
+        
+      
+     
+     
+    document.getElementById("userTable").style.display = "table";
+
+    document.getElementById("nameCell").innerText = name;
+    document.getElementById("emailCell").innerText = email;
+    document.getElementById("numberCell").innerText = number;
+    document.getElementById("cityCell").innerText = city;
+    document.getElementById("bloodcell").innerText=blood;
+    document.getElementById("native").innerText=native;
+     document.getElementById("pob").innerText=pob;
+     document.getElementById("dob").innerText=dob;
+     document.getElementById("sslcmark").innerText=sslcmark;
+     document.getElementById("mark").innerText=mark;
+     
+      
+
+     
